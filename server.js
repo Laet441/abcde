645c21af0b48e58fc260f21337cb56e7cd3d198e
Laet441 <lightdeveloperru@gmail.com>
2025-02-02 18:02:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 // JSON Server module
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+
+const fs = require('fs'); const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')));
+const router = jsonServer.router(db);
+
+// const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
